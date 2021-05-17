9c41329ba1d2345b1cfd7434309b7257c3179af8
Ryuichi Kawano <zan56011@nifty.com>
2021-05-17 10:19:24 +0900

    スケルトンプログラムの作成まで

---
+++ SampleCode/SampleCode/main.cpp
@@ -0,0 +1,13 @@
+#include<DxLib.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	ChangeWindowMode(true);
+	DxLib_Init();
+	SetDrawScreen(DX_SCREEN_BACK);
+	while (ProcessMessage() != -1) {
+		ClearDrawScreen();
+		ScreenFlip();
+	}
+	DxLib_End();
+	return 0;
+}
